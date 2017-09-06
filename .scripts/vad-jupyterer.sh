d8e91a2fd4e20c317ba49c787afab47e48f96a21
vadegg <vadim.glazkov@gmail.com>
2017-09-06 21:10:27 +0300

    jupyter autostartup added

---
+++ .scripts/vad-jupyterer.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+tmux kill-session -t jupyter >> /dev/null;
+tmux new-session -d -s jupyter >> /dev/null;
+tmux send-keys -t jupyter "cd $1" Enter
+tmux send-keys -t jupyter ". .activate_venv.sh" Enter
+tmux send-keys -t jupyter "jupyter notebook" Enter
+echo -e "$(tput setaf 22)Jupyter server started successfully \033[0m"
